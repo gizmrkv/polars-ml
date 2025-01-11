d37111ee02b0cba46723cad6f7d4e369e5b2d5ea
gizmrkv <gizmrkv@gmail.com>
2025-01-12 01:13:40 +0900

    Add: LabelEncoding

--- src/polars_ml/transformer.py
+++ src/polars_ml/transformer.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Iterable, Self
+from typing import Any, Iterable, Mapping, Self, Sequence
 
 import polars as pl
 import polars.selectors as cs
-from polars import DataFrame
+from polars import DataFrame, Series
 from polars._typing import IntoExpr
 
 from polars_ml import Component
@@ -21,7 +21,11 @@ class MoveScaler(Component, ABC):
     @abstractmethod
     def get_scales(self, data: DataFrame) -> dict[str, float]: ...
 
-    def fit(self, data: DataFrame) -> Self:
+    def fit(
+        self,
+        data: DataFrame,
+        validation_data: DataFrame | Mapping[str, DataFrame] | None = None,
+    ) -> Self:
         data = data.select(*self.exprs)
         self.moves = self.get_moves(data)
         self.scales = self.get_scales(data)
@@ -36,6 +40,17 @@ class MoveScaler(Component, ABC):
         )
         return data.with_columns(
             [(pl.col(col) - self.moves[col]) / self.scales[col] for col in columns]
+        )
+
+    def inverse_transform(self, data: DataFrame) -> DataFrame:
+        columns = (
+            data.lazy()
+            .select(cs.matches(r"|".join(self.moves.keys())))
+            .collect_schema()
+            .names()
+        )
+        return data.with_columns(
+            [(pl.col(col) * self.scales[col] + self.moves[col]) for col in columns]
         )
 
 
@@ -64,9 +79,9 @@ class QuantileScaler(MoveScaler):
         super().__init__(*expr)
         self.quantile = quantile
         q1, q3 = quantile
-        assert (
-            0 <= q1 < q3 <= 1
-        ), f"Quantile values must be in the range [0, 1] and q1 < q3. Got {quantile}"
+        assert 0 <= q1 < q3 <= 1, (
+            f"Quantile values must be in the range [0, 1] and q1 < q3. Got {quantile}"
+        )
 
     def get_moves(self, data: DataFrame) -> dict[str, float]:
         return data.quantile(0.5).row(0, named=True)
@@ -75,4 +90,61 @@ class QuantileScaler(MoveScaler):
         q1, q3 = self.quantile
         return data.select(pl.all().quantile(q3) - pl.all().quantile(q1)).row(
             0, named=True
+        )
+
+
+class LabelEncoding(Component):
+    def __init__(
+        self,
+        *exprs: IntoExpr | Iterable[IntoExpr],
+        orders: dict[str, Sequence[Any]] | None = None,
+        maintain_order: bool = False,
+    ):
+        self.exprs = exprs
+        self.orders = orders or {}
+        self.maintain_order = maintain_order
+
+    def fit(
+        self,
+        data: DataFrame,
+        validation_data: DataFrame | Mapping[str, DataFrame] | None = None,
+    ) -> Self:
+        data = data.select(*self.exprs)
+        self.mappings = {
+            col: DataFrame(
+                [
+                    Series(col, self.orders[col]),
+                    Series("label", range(len(self.orders[col])), dtype=pl.UInt32),
+                ]
+            )
+            if col in self.orders
+            else (
+                data.select(col)
+                .unique(maintain_order=self.maintain_order)
+                .drop_nulls()
+                .with_row_index("label")
+            )
+            for col in data.columns
+        }
+
+        return self
+
+    def transform(self, data: DataFrame) -> DataFrame:
+        return data.with_columns(
+            [
+                data.select(col).join(mapping, on=col, how="left")["label"].rename(col)
+                for col, mapping in self.mappings.items()
+                if col in data.columns
+            ]
+        )
+
+    def inverse_transform(self, data: DataFrame) -> DataFrame:
+        return data.with_columns(
+            [
+                data.select(pl.col(col).alias("label"))
+                .join(mapping, on="label", how="left")[col]
+                .rename(col)
+                for col, mapping in self.mappings.items()
+                if col in data.columns
+            ]
         )
