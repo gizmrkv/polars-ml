18b40096e27e5d57014cfddbcc6dde73fd2ead5d
gizmrkv <gizmrkv@gmail.com>
2025-01-13 00:00:39 +0900

    Add: get_country_codes, get_country_holidays

--- src/polars_ml/utils.py
+++ src/polars_ml/utils.py
@@ -104,3 +104,31 @@ class GroupByThen(Component):
 
     def transform(self, data: DataFrame) -> DataFrame:
         return data.join(self.grouped, on=self.by, how="left")
+
+
+def get_country_codes() -> DataFrame:
+    import pycountry
+
+    columns = ["name", "alpha_2", "alpha_3", "numeric", "flag"]
+    return DataFrame(
+        {
+            column: [getattr(country, column) for country in pycountry.countries]
+            for column in columns
+        }
+    )
+
+
+def get_country_holidays(
+    countries: str | Iterable[str], years: int | Iterable[int]
+) -> DataFrame:
+    import holidays
+
+    return pl.concat(
+        [
+            pl.DataFrame({"date": dates, "holiday": names, "country": country})
+            for country in countries
+            for dates, names in [
+                zip(*holidays.country_holidays(country, years=years).items())
+            ]
+        ]
+    )
