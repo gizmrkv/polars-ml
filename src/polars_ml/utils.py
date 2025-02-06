5defbb848f0c9ae70ebc1365d506bdba0eebfb76
gizmrkv <gizmrkv@gmail.com>
2025-02-06 23:59:47 +0900

    Add: train_test_split

--- src/polars_ml/utils.py
+++ src/polars_ml/utils.py
@@ -1,10 +1,42 @@
 import random
+import uuid
 from typing import Iterable, Iterator, Literal, TypeVar
 
 import polars as pl
 from polars import DataFrame
 
 T = TypeVar("T")
+
+
+def train_test_split(
+    data: DataFrame,
+    test_size: float,
+    *,
+    stratify: str | None = None,
+    shuffle: bool = True,
+    seed: int | None = None,
+) -> tuple[list[int], list[int]]:
+    index_name = uuid.uuid4().hex
+    if stratify is None:
+        data = data.with_row_index(index_name).select(
+            index_name,
+            pl.col(index_name).alias("is_train") < data.height * test_size,
+        )
+    else:
+        data = data.with_row_index(index_name).select(
+            index_name,
+            stratify or pl.lit(0),
+            pl.cum_count(stratify).over(stratify).alias("is_train")
+            <= pl.len().over(stratify) * test_size,
+        )
+
+    data = data.with_columns(
+        pl.col("is_train").shuffle(seed).over(stratify) if shuffle else pl.all()
+    )
+    return (
+        data.filter(pl.col("is_train"))[index_name].to_list(),
+        data.filter(~pl.col("is_train"))[index_name].to_list(),
+    )
 
 
 def get_country_codes() -> DataFrame:
