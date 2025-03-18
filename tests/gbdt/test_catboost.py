1bcbadeead42366dce135409dc8494c7ec295834
gizmrkv <gizmrkv@gmail.com>
2025-03-18 23:56:18 +0900

    Fix

--- tests/gbdt/test_catboost.py
+++ tests/gbdt/test_catboost.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import polars as pl
 from polars import DataFrame
 
@@ -8,16 +10,17 @@ from polars_ml.pipeline.testing import assert_component_valid
 def test_pipeline_catboost_simple_binary_classification(
     test_data_simple_binary_classification: DataFrame,
 ):
-    assert_component_valid(
-        Pipeline()
-        .gbdt.catboost(
-            pl.exclude("y"),
-            "y",
-            {"objective": "Logloss", "iterations": 10},
+    with tempfile.TemporaryDirectory() as tmpdir:
+        assert_component_valid(
+            Pipeline()
+            .gbdt.catboost(
+                pl.exclude("y"),
+                "y",
+                {"objective": "Logloss", "iterations": 10, "train_dir": tmpdir},
+            )
+            .select(
+                (pl.col("y") == pl.col("catboost").gt(0.5)).mean().round(1).alias("acc")
+            ),
+            test_data_simple_binary_classification,
+            DataFrame({"acc": 1.0}),
         )
-        .select(
-            (pl.col("y") == pl.col("catboost").gt(0.5)).mean().round(1).alias("acc")
-        ),
-        test_data_simple_binary_classification,
-        DataFrame({"acc": 1.0}),
-    )
