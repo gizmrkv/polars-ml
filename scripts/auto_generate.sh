24dc2c6fafd42f507db0cb87fe5da436fe2b6b05
gizmrkv <gizmrkv@gmail.com>
2025-12-21 23:47:02 +0900

    Delet

--- scripts/auto_generate.sh
+++ scripts/auto_generate.sh
@@ -2,5 +2,4 @@ uv run python scripts/insert_group_by_methods.py
 uv run python scripts/insert_gbdt_methods.py
 uv run python scripts/insert_metrics_methods.py
 uv run python scripts/insert_pipeline_methods.py
-uv run python scripts/insert_pipeline_tests.py
 uv format
