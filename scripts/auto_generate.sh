14ce3ba152a03f5d8579bc03a67884b98d8803a4
gizmrkv <gizmrkv@gmail.com>
2025-12-21 22:56:48 +0900

    Add

--- scripts/auto_generate.sh
+++ scripts/auto_generate.sh
@@ -1,4 +1,6 @@
 uv run python scripts/insert_group_by_methods.py
+uv run python scripts/insert_gbdt_methods.py
 uv run python scripts/insert_metrics_methods.py
 uv run python scripts/insert_pipeline_methods.py
+uv run python scripts/insert_pipeline_tests.py
 uv format
