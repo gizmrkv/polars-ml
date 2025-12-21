59bb1042a1bdefcbc688c6ede40d6e856d861c91
gizmrkv <gizmrkv@gmail.com>
2025-12-22 00:00:41 +0900

    Add: test

---
+++ tests/polars_ml/pipeline/test_getattr.py
@@ -0,0 +1,37 @@
+import polars as pl
+import pytest
+from polars import DataFrame
+from polars.testing import assert_frame_equal
+
+from polars_ml.pipeline.getattr import GetAttr, GetAttrPolars
+
+
+@pytest.fixture
+def sample_df() -> DataFrame:
+    return pl.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+
+
+class TestGetAttr:
+    def test_fit_transform(self, sample_df: DataFrame):
+        t = GetAttr("select", pl.col("a") * 2)
+        t.fit(sample_df)
+
+        output = t.transform(sample_df)
+        expected = sample_df.select(pl.col("a") * 2)
+        assert_frame_equal(output, expected)
+
+        output = t.fit_transform(sample_df)
+        assert_frame_equal(output, expected)
+
+
+class TestGetAttrPolars:
+    def test_fit_transform(self, sample_df: DataFrame):
+        t = GetAttrPolars("concat", [sample_df, sample_df])
+        t.fit(sample_df)
+
+        output = t.transform(sample_df)
+        expected = pl.concat([sample_df, sample_df])
+        assert_frame_equal(output, expected)
+
+        output = t.fit_transform(sample_df)
+        assert_frame_equal(output, expected)
